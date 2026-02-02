f839c02212dcc4f30f43be6e3d868e1f6d0005ce
ashifvet <ashifvet@gmail.com>
2026-02-02 21:16:15 +0600

    list.dart

---
+++ .idea/list.dart
@@ -0,0 +1,10 @@
+void main(){
+  List<String> listName = ['Rahim', 'Karim', 'Helal', 'Belal', 'Asif', 'Arif'];
+  print(listName.length);
+  int i;
+  for(i=0; i < listName.length; i++){
+    print(listName[i]);
+  }
+  print('\n');
+  print('End');
+}
